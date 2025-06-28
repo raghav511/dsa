aad1e55182a02db4f2fdb87427cfa7c69a23f641
ballur raghavendra raju <raghav0611@gmail.com>
2025-06-28 20:43:49 +0530

    day4 redo later- warmup star pattern

--- warm-up/star_pattern_end.py
+++ warm-up/star_pattern_end.py
@@ -1,3 +1,10 @@
-for i in range(0,6,):
-    for j in range(i+1):
-        print(i*" ",j*"*")
+# n=5  first row=0 i, space j=4 n-(i+1), star k = i+1
+
+n=5
+for i in range(n):
+    space = "_"
+    for j in range(n-(i+1)):
+        space=j*space
+    for k in range(i+1):
+        star = k*"*"
+    print(space,star)
